34a412970226efbc656d64b9dd36d3c1b3dd2571
xlui <liuqi0315@gmail.com>
2018-11-01 15:32:37 +0800

    Update git ignore

---
+++ src/test/java/app/xlui/target/TargetApplicationTests.java
@@ -0,0 +1,16 @@
+package app.xlui.target;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TargetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
