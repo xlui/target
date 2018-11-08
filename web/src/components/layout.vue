00849b2e481930808582c692998311458abe21b9
xlui <liuqi0315@gmail.com>
2018-11-08 15:08:48 +0800

    Simple layout

---
+++ web/src/components/layout.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="container">
+    <section class="menu"></section>
+    <section class="content-container"></section>
+  </section>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="less">
+  @import "../common/style/layouts.less";
+</style>
