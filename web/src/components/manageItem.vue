134d9a63aae48e2b49f44856fbd421ea24656607
xlui <liuqi0315@gmail.com>
2018-11-25 16:18:49 +0800

    Feature: show target details in manage page

--- web/src/components/manageItem.vue
+++ web/src/components/manageItem.vue
@@ -1,14 +1,61 @@
 <template>
   <div>
-    <a href="#" @click="showCheckIn">
+    <a href="#" @click="showModal = true">
       <div class="target" :style="{background: bgColor}">
         <div class="title">{{ target.title }}</div>
         <div class="desc">{{ target.description }}</div>
       </div>
     </a>
-    <modal v-if="showModal" @close="showModal = false">
-      <!-- todo: show target details here -->
-      <p slot="body">This is the details page of the checkin</p>
+    <modal class="manage" v-if="showModal" @close="showModal = false">
+      <h2 slot="header">{{ target.title }}</h2>
+      <form role="form" class="form-horizontal" slot="body">
+        <div class="form-group form-item">
+          <label for="title" class="col-sm-offset-1 col-sm-3">Title:</label>
+          <div class="col-sm-7">
+            <input type="text" id="title" class="form-control" v-model="target.title"/>
+          </div>
+        </div>
+        <div class="form-group form-item">
+          <label for="description" class="col-sm-offset-1 col-sm-3">Description:</label>
+          <div class="col-sm-7">
+            <input type="text" id="description" class="form-control" v-model="target.description"/>
+          </div>
+        </div>
+        <div class="form-group form-item">
+          <label for="startDate" class="col-sm-offset-1 col-sm-3">Start date:</label>
+          <div class="col-sm-7">
+            <input type="date" id="startDate" class="form-control" v-model="target.startDate"/>
+          </div>
+        </div>
+        <div class="form-group form-item">
+          <label for="endDate" class="col-sm-offset-1 col-sm-3">End date:</label>
+          <div class="col-sm-7">
+            <input type="date" id="endDate" class="form-control" v-model="target.endDate"/>
+          </div>
+        </div>
+        <div class="form-group form-item">
+          <label for="checkinStart" class="col-sm-offset-1 col-sm-3">Check in start:</label>
+          <div class="col-sm-7">
+            <input type="time" id="checkinStart" class="form-control" v-model="target.checkinStart"/>
+          </div>
+        </div>
+        <div class="form-group form-item">
+          <label for="checkinEnd" class="col-sm-offset-1 col-sm-3">Check in end:</label>
+          <div class="col-sm-7">
+            <input type="time" id="checkinEnd" class="form-control" v-model="target.checkinEnd"/>
+          </div>
+        </div>
+        <div class="form-group form-item">
+          <label for="repeat" class="col-sm-offset-1 col-sm-3">Repeat:</label>
+          <div class="col-sm-7">
+            <input type="text" id="repeat" class="form-control" v-model="target.repeat"/>
+          </div>
+        </div>
+      </form>
+      <div slot="footer">
+        <button type="button" class="btn btn-primary" @click="updateTarget">提交</button>
+        <button type="button" class="btn btn-default" @click="showModal = false">关闭</button>
+      </div>
     </modal>
   </div>
 </template>
@@ -30,8 +77,8 @@
       }
     },
     methods: {
-      showCheckIn() {
-        this.showModal = true;
+      updateTarget() {
+        console.log('click update target...')
       }
     },
     components: {
