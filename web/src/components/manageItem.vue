4a9d96bea448474181e76a2f2bd226b754d47302
xlui <liuqi0315@gmail.com>
2018-12-17 13:09:54 +0800

    Refactor: update manage item ui

--- web/src/components/manageItem.vue
+++ web/src/components/manageItem.vue
@@ -8,47 +8,57 @@
     </a>
     <el-dialog :title="target.title" :visible.sync="showDialog">
       <el-form ref="form" label-position="left" label-width="150px">
-        <el-form-item label="Title">
-          <el-col :span="18">
+        <el-col :span="23" :offset="1">
+          <el-form-item label="Title">
             <el-input v-model="target.title"></el-input>
-          </el-col>
-        </el-form-item>
-        <el-form-item label="Description">
-          <el-col :span="18">
+          </el-form-item>
+        </el-col>
+        <el-col :span="23" :offset="1">
+          <el-form-item label="Description">
             <el-input v-model="target.description"></el-input>
-          </el-col>
-        </el-form-item>
-        <el-form-item label="Start date">
-          <el-col :span="9">
-            <el-date-picker type="date" v-model="target.startDate"></el-date-picker>
-          </el-col>
-        </el-form-item>
-        <el-form-item label="End date">
-          <el-col :span="9">
-            <el-date-picker type="date" v-model="target.endDate"></el-date-picker>
-          </el-col>
-        </el-form-item>
-        <el-form-item label="Check in start">
-          <el-col :span="9">
-            <el-time-picker type="fixed-time" value-format="HH:mm:ss" v-model="target.checkinStart"></el-time-picker>
-          </el-col>
-        </el-form-item>
-        <el-form-item label="Check in end">
-          <el-col :span="9">
-            <el-time-picker type="fixed-time" value-format="HH:mm:ss" v-model="target.checkinEnd"></el-time-picker>
-          </el-col>
-        </el-form-item>
-        <el-form-item label="Repeat">
-          <el-checkbox-group v-model="target.repeat" class="el-col el-col-19">
-            <el-checkbox label="64">Sun</el-checkbox>
-            <el-checkbox label="32">Mon</el-checkbox>
-            <el-checkbox label="16">Tue</el-checkbox>
-            <el-checkbox label="8">Wed</el-checkbox>
-            <el-checkbox label="4">Thu</el-checkbox>
-            <el-checkbox label="2">Fri</el-checkbox>
-            <el-checkbox label="1">Sat</el-checkbox>
-          </el-checkbox-group>
-        </el-form-item>
+          </el-form-item>
+        </el-col>
+        <el-col :span="23" :offset="1">
+          <el-form-item label="Start date">
+            <el-col :span="6">
+              <el-date-picker type="date" v-model="target.startDate"></el-date-picker>
+            </el-col>
+          </el-form-item>
+        </el-col>
+        <el-col :span="23" :offset="1">
+          <el-form-item label="End date">
+            <el-col :span="6">
+              <el-date-picker type="date" v-model="target.endDate"></el-date-picker>
+            </el-col>
+          </el-form-item>
+        </el-col>
+        <el-col :span="23" :offset="1">
+          <el-form-item label="Check in start">
+            <el-col :span="6">
+              <el-time-picker type="fixed-time" value-format="HH:mm:ss" v-model="target.checkinStart"></el-time-picker>
+            </el-col>
+          </el-form-item>
+        </el-col>
+        <el-col :span="23" :offset="1">
+          <el-form-item label="Check in end">
+            <el-col :span="6">
+              <el-time-picker type="fixed-time" value-format="HH:mm:ss" v-model="target.checkinEnd"></el-time-picker>
+            </el-col>
+          </el-form-item>
+        </el-col>
+        <el-col :span="23" :offset="1">
+          <el-form-item label="Repeat">
+            <el-checkbox-group v-model="target.repeat">
+              <el-checkbox label="64">Sun</el-checkbox>
+              <el-checkbox label="32">Mon</el-checkbox>
+              <el-checkbox label="16">Tue</el-checkbox>
+              <el-checkbox label="8">Wed</el-checkbox>
+              <el-checkbox label="4">Thu</el-checkbox>
+              <el-checkbox label="2">Fri</el-checkbox>
+              <el-checkbox label="1">Sat</el-checkbox>
+            </el-checkbox-group>
+          </el-form-item>
+        </el-col>
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button size="medium" @click="showDialog = false">关闭</el-button>
